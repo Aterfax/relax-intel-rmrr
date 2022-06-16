dd5121d6084942a750ed17c155e2400375058a72
Aterfax <Aterfax@users.noreply.github.com>
2022-06-16 19:48:32 +0100

    Ensured branch changes apply with cache repo
    See: https://stackoverflow.com/a/45322343

--- build/proxmox/build.sh
+++ build/proxmox/build.sh
@@ -54,6 +54,8 @@ if [[ -d "proxmox-kernel" ]]; then
     git clean -xfd
     git submodule foreach --recursive git clean -xfd
     git reset --hard
+    git pull
+    git checkout ${PVE_KERNEL_BRANCH}
     git submodule foreach --recursive git reset --hard
     git submodule update --init --recursive
     PVE_KERNEL_GIT_DIR_PRESENT=1
@@ -111,7 +113,7 @@ cd proxmox-kernel
 echo "Step 2.1: Downloading Proxmox kernel toolchain & patches"
 
 if [[ $PVE_KERNEL_GIT_DIR_PRESENT -ne 1 ]]; then
-  git clone --depth=1 -b ${PVE_KERNEL_BRANCH} git://git.proxmox.com/git/pve-kernel.git
+  git clone ${PVE_KERNEL_BRANCH} git://git.proxmox.com/git/pve-kernel.git
 fi
 
 if [[ $RELAX_INTEL_RMRR_GIT_DIR_PRESENT -ne 1 ]]; then
